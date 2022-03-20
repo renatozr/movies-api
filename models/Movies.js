5ee4265967b407c724086b3721ca9b385673720c
renatozr <renatozr07@gmail.com>
2022-03-20 12:01:38 -0300

    feature: update model, test: update model

--- models/Movies.js
+++ models/Movies.js
@@ -15,7 +15,26 @@ const getAll = async () => {
   return movies;
 };
 
+const getById = async (id) => {
+  const [movie] = await connection.execute('SELECT * FROM movies WHERE id = ?;', [id]);
+
+  if (!movie[0]) return null;
+
+  return movie[0];
+};
+
+const update = async (id, title, directedBy, releaseYear) => {
+  const [movie] = await connection.execute(
+    'UPDATE movies SET title = ?, directed_by = ?, release_year = ? WHERE id = ?;',
+    [title, directedBy, releaseYear, id],
+  );
+
+  return movie[0];
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
+  update
 };
--- tests/models/Movies.test.js
+++ tests/models/Movies.test.js
@@ -27,3 +27,112 @@ describe('O método "create" de MoviesModel', () => {
     });
   });
 });
+
+describe('O método "getAll" de MoviesModel', () => {
+  describe('quando pega os filmes no banco de dados', () => {
+    const moviesPayload = [
+      {
+        id: 1,
+        title: "Um sonho de liberdade",
+        directed_by: "Frank Darabont",
+        release_year: 1994
+      },
+      {
+        id: 2,
+        title: "O Poderoso Chefão",
+        directed_by: "Francis Ford Coppola",
+        release_year: 1972
+      },
+      {
+        id: 3,
+        title: "Batman: O Cavaleiro das Trevas",
+        directed_by: "Christopher Nolan",
+        release_year: 2008
+      }
+    ];
+
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([moviesPayload]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna um array com os filmes', async () => {
+      const response = await MoviesModel.getAll();
+
+      expect(response).to.be.equal(moviesPayload);
+    });
+  });
+});
+
+describe('O método "getById" de MoviesModel', () => {
+  describe('quando pega o filme no banco de dados', () => {
+    const moviePayload = [{
+        id: 1,
+        title: "Um sonho de liberdade",
+        directed_by: "Frank Darabont",
+        release_year: 1994
+    }];
+    const movieId = 1;
+
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([moviePayload]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna o filme', async () => {
+      const response = await MoviesModel.getById(movieId);
+
+      expect(response).to.be.equal(moviePayload[0]);
+    });
+  });
+
+  describe('quando o filme não existe no banco de dados', () => {
+    const movieId = 999;
+
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([[]]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna o filme', async () => {
+      const response = await MoviesModel.getById(movieId);
+
+      expect(response).to.be.equal(null);
+    });
+  });
+});
+
+describe('O método "update" de MoviesModel', () => {
+  describe('quando atualiza o filme no banco de dados', () => {
+    const moviePayload = [{
+        id: 1,
+        title: "Um sonho de liberdade",
+        directed_by: "Frank Darabont",
+        release_year: 1994
+    }];
+    const movieId = 1;
+
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([moviePayload]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna o filme', async () => {
+      const response = await MoviesModel.update(movieId);
+
+      expect(response).to.be.equal(moviePayload[0]);
+    });
+  });
+});
