cf72c1cecd9d5a6e3adf55f9265feda9053ab828
renatozr <renatozr07@gmail.com>
2022-03-25 13:59:20 -0300

    bug: listen port

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const MoviesRouter = require('./routers/Movies');
-const error = require('./middlewares/error');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const MoviesRouter = require("./routers/Movies");
+const error = require("./middlewares/error");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use('/movies', MoviesRouter);
+app.use("/movies", MoviesRouter);
 
 app.use(error);
 
 app.listen(PORT, () => {
-  console.log(`Ouvindo na porta ${3000}`);
+  console.log(`Ouvindo na porta ${PORT}`);
 });
